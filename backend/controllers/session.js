22eff89d89da35bb9b352000a7915052e7eb7c1e
tristan-lorenzo21 <tristan.lorenzo21@gmail.com>
2023-03-24 11:20:09 -0700

    Deleted duplicate function

--- backend/controllers/session.js
+++ backend/controllers/session.js
@@ -39,32 +39,6 @@ const newSession = (req, res) => {
     })
 }
 
-const addQuestionToTopic = (req, res) => {
-    const { id } = req.params;
-    const { studentId, question, name, topic } = req.body;
-
-    Session.findById(id)
-        .then((session) => {
-            const groupIndex = session.groups.findIndex((group) => group.topic === topic);
-
-            if (groupIndex !== -1) {
-                session.groups[groupIndex].studentQuestions.push({ studentId: studentId, question: question, name: name });
-            } else {
-                session.groups.push({ topic, studentQuestions: [{ studentId: studentId, question: question, name: name }] });
-            }
-
-            return session.save();
-        })
-        .then((updatedSession) => {
-            res.json(updatedSession);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json({ error: 'Could not add student question to group.' });
-        });
-
-}
-
 const search = (req, res) => {
     const professorName = req.query.professorName;
     const className = req.query.className;
@@ -89,7 +63,6 @@ const search = (req, res) => {
     })
 
 }
-
 
 const addQuestionToTopic = async (req, res) => {
     const { id } = req.params;
