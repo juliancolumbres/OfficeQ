b74c6d37bfc121341ea9ede6737daad7982fbe7b
vanessaucey <vanessa.vega@sjsu.edu>
2023-05-05 10:32:03 -0700

    updates on root
    Still needs more changes to look aesthetically pleasing.

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -3,7 +3,7 @@ import './Root.css';
 
 export default function Root() {
   return (
-    <div>
+    <div className="welcomeBody">
       <h1 className="welcome">
         {"Welcome to"}
         <span class="logo" style={{ display: "inline-block" }}>
@@ -11,12 +11,14 @@ export default function Root() {
         </span>
       </h1>
      
+      <div class="button-container">
       <button className= "studentBttn">
         <Link to={'/student'} className="button">Are you a student?</Link>
       </button>
       <button className= "profBttn">
         <Link to={'/professor'} className="button">Are you a professor?</Link>
       </button>
+      </div>
     </div>
   );
 }
