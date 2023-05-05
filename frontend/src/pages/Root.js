f7b117fc6804b2eda9efffe12dffc13fefaf8252
Travis Cheng <travischeng03@gmail.com>
2023-05-05 08:57:42 -0700

    changed logo for root page

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Root() {
     return (
         <div>
-            <h1>OfficeQ</h1>
+            <span class = "logo">Office<span class="colored-letter">Q</span></span>
             <Link to={'/student'}>Are you a student?</Link>
             <Link to={'/professor'}>Are you a professor?</Link>
         </div>
