2ff37e2008c9ba2a2e5b85efad750c5d1a8344ad
Travis Cheng <travischeng03@gmail.com>
2023-05-05 09:32:10 -0700

    replaced React App title to OfficeQ

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,22 @@ import './index.css';
 import App from './App.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+  () => {
+    document.title = 'OfficeQ';
+  }
+);
+
 
